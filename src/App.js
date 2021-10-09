1a15675a78022dec6831c0acac5f5f547f5238ae
Istiak001 <istiakmahmud1010@gmail.com>
2021-10-09 17:53:00 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router ,Switch,Route} from 'react-router-dom';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Blog from './Components/Blog/Blog';
+import About from './Components/About/About';
+import Error from './Components/Error/Error';
+import HomeService from './Components/HomeService/HomeService';
+import Courses from './Components/Courses/Courses';
+import HomeUi from './Components/HomeUi/HomeUi';
+import BlogUi from './Components/BlogUI/BlogUi';
+import ServiceUi from './Components/ServiceUi/ServiceUi';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //creating router
+    <Router>
+      <Header></Header>
+
+      <Switch>     
+        <Route path="/home">
+          <HomeUi></HomeUi>
+            <HomeService></HomeService>  
+        </Route>
+
+        <Route path="/service">
+          <ServiceUi></ServiceUi>
+          <Courses></Courses>       
+        </Route>
+
+        <Route path="/blog">
+          <BlogUi></BlogUi>
+          <Blog></Blog>
+        </Route>
+
+        <Route path="/about">
+        <About></About>
+        </Route>
+
+          <Route exact path="/">
+             <HomeUi></HomeUi>
+            <HomeService></HomeService> 
+        </Route>
+        
+          <Route path="*">
+             <Error></Error>
+        </Route>
+        
+        </Switch>
+      <Footer></Footer>
+    </Router>
+    
   );
 }
 
